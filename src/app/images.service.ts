d919e15a519418668c9c24adb53ae0d49c77b019
Manuele Dones <manuele.dones@sorint.it>
2019-08-29 16:45:00 +0200

    Remove X-Host-Override request header

--- src/app/images.service.ts
+++ src/app/images.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
@@ -10,7 +10,7 @@ export class ImagesService {
   constructor(private httpClient: HttpClient) { }
 
   getStats(url: string) {
-    const headers = new HttpHeaders().set('X-Host-Override', 'inception-fn.default.example.com');
-    return this.httpClient.get<any[]>(`${environment.url}?imageUrl=${url}`, { headers });
+    // const headers = new HttpHeaders().set('X-Host-Override', 'inception-fn.default.example.com');
+    return this.httpClient.get<any[]>(`${environment.url}?imageUrl=${url}`/*, { headers }*/);
   }
 }
